ec5424ecbcc61eb29f49e51ade07f34a8ab221e0
Gabriele Tomè <tome.gabriele@gmail.com>
2025-04-03 15:32:35 +0200

    Add STRINGdb for javascript rendering in shiny

--- R/STRING_analysis.R
+++ R/STRING_analysis.R
@@ -24,7 +24,7 @@
 #' }
 STRINGdb_network <- function(differential_results, species="Homo sapiens", 
                              dirOutput="results_ProTN", subfold_Fig="pics", subfold_net="STRINGdb",
-                             phospho_ctrl = FALSE){
+                             phospho_ctrl = FALSE, score_thr = 700, shiny=FALSE){
   #Read Taxonomy
   codtax <- fread(system.file("extdata", "subset_tax.csv", package = "proTN"))
   tryCatch({
@@ -62,7 +62,11 @@ STRINGdb_network <- function(differential_results, species="Homo sapiens",
   }
   
   if (!is.null(dirOutput_net)) {
-    stringdb_results <- process_string_network(g_sel_comp = res$g_sel_comp, dirOutput_net, taxonomy_NCBI)
+    if(shiny){
+      stringdb_results <- process_string_network_shiny(g_sel_comp = res$g_sel_comp, dirOutput_net, taxonomy_NCBI, score_thr)
+    } else{
+      stringdb_results <- process_string_network(g_sel_comp = res$g_sel_comp, dirOutput_net, taxonomy_NCBI, score_thr)
+    }
     return(stringdb_results)
   }else{
     stop("Error in creating output directory")
@@ -88,9 +92,9 @@ select_regulated_genes <- function(deps_l_df) {
 }
 
 # Function to process STRING network for each comparison
-process_string_network <- function(g_sel_comp, dirOutput_net, taxonomy_NCBI) {
+process_string_network <- function(g_sel_comp, dirOutput_net, taxonomy_NCBI, score_thr = 700) {
   string_folder <- system.file("extdata", package = "proTN")
-  string_db <- STRINGdb$new(version="12", species=taxonomy_NCBI, score_threshold=500, input_directory=string_folder)
+  string_db <- STRINGdb$new(version="12", species=taxonomy_NCBI, score_threshold=score_thr, input_directory=string_folder)
   
   stringdb_results <- list()
   for (comp in names(g_sel_comp)) {
@@ -105,13 +109,39 @@ process_string_network <- function(g_sel_comp, dirOutput_net, taxonomy_NCBI) {
     string_gene_df <- unique(data.table(gene1 = links_string$from, gene2 = links_string$to, weight = links_string$combined_score))
     if (nrow(string_gene_df) > 0) {
       fwrite(string_gene_df, file = paste0(dirOutput_net, "/", gsub(comp, pattern = "\\/", replacement="vs"), "_connection.txt"), sep = "\t")
-      link <- paste0("https://string-db.org/cgi/network?identifiers=", paste(unique(string_mapped$STRING_id), collapse = "%0d"), "&species=", taxonomy_NCBI, "&required_score=500")
-      string_db$plot_network(string_mapped, required_score = 500)
+      link <- paste0("https://string-db.org/cgi/network?identifiers=", paste(unique(string_mapped$STRING_id), collapse = "%0d"), "&species=", taxonomy_NCBI, "&required_score=",score_thr)
+      string_db$plot_network(string_mapped, required_score = score_thr)
       pdf(file = paste0(dirOutput_net,"/", comp, "_network.pdf"), width = 150, height = 150)
-      string_db$plot_network(string_mapped, required_score = 500)
+      string_db$plot_network(string_mapped, required_score = score_thr)
       dev.off()
       
       stringdb_results[[comp]] <- string_gene_df
+    } else {
+      stop("No strong interaction detected between the proteins. Usually too few proteins.")
+    }
+  }
+  return(stringdb_results)
+}
+
+# Function to process STRING network for each comparison in shiny
+process_string_network_shiny <- function(g_sel_comp, dirOutput_net, taxonomy_NCBI, score_thr = 700) {
+  string_folder <- system.file("extdata", package = "proTN")
+  string_db <- STRINGdb$new(version="12", species=taxonomy_NCBI, score_threshold=score_thr, input_directory=string_folder)
+  
+  stringdb_results <- list()
+  for (comp in names(g_sel_comp)) {
+    gene_name <- unique(g_sel_comp[[comp]])
+    setnames(gene_name, "id", "gene_id")
+    string_mapped <- string_db$map(gene_name, "gene_id", removeUnmappedRows = TRUE)
+    links_string <- as.data.table(string_db$get_interactions(string_mapped$STRING_id))
+    links_string[, `:=`(
+      from = string_mapped$gene_id[match(from, string_mapped$STRING_id)],
+      to = string_mapped$gene_id[match(to, string_mapped$STRING_id)]
+    )]
+    string_gene_df <- unique(data.table(gene1 = links_string$from, gene2 = links_string$to, weight = links_string$combined_score))
+    if (nrow(string_gene_df) > 0) {
+      fwrite(string_gene_df, file = paste0(dirOutput_net, "/", gsub(comp, pattern = "\\/", replacement="vs"), "_connection.txt"), sep = "\t")
+      stringdb_results[[comp]] <- paste(unique(string_mapped$STRING_id), collapse = "%0d")
     } else {
       stop("No strong interaction detected between the proteins. Usually too few proteins.")
     }
