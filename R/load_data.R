c0880bb3ed7b509d752032123c6fd22eb75bda51
Gabriele Tomè <tome.gabriele@gmail.com>
2025-04-22 18:02:05 +0200

    Add dependencies

--- R/load_data.R
+++ R/load_data.R
@@ -61,7 +61,7 @@ extract_example = function(path_phospho = NULL,
 #' @importFrom dplyr ungroup mutate filter group_by n
 #' @import data.table
 #' @import readxl
-#' @importFrom biomaRt useEnsembl getBM
+#' @importFrom biomaRt useEnsembl getBM listDatasets
 #' @export
 read_proteomics <- function(software, folder, annotation_filename = "annotation",
                             peptide_filename = "pep", proteinGroup_filename = "prot", 
@@ -601,7 +601,7 @@ read_PD_files <- function(anno_filename, pep_filename, prot_filename,
 #' @importFrom dplyr ungroup mutate filter group_by n
 #' @import data.table
 #' @import readxl
-#' @importFrom biomaRt useEnsembl getBM
+#' @importFrom biomaRt useEnsembl getBM listDatasets
 #' @export
 read_phosphoproteomics <- function(software, folder, 
                                    keep_only_phosphomodification = T, phospho_thr = 0.75, 
@@ -1340,7 +1340,7 @@ read_phospho_PD_files <- function(anno_filename, pep_filename, prot_filename, ps
 #' @importFrom dplyr ungroup mutate filter group_by n
 #' @import data.table
 #' @import readxl
-#' @importFrom biomaRt useEnsembl getBM
+#' @importFrom biomaRt useEnsembl getBM listDatasets
 #' @export
 read_phospho_proteome_proteomics <- function(software, 
                                              folder_proteome, folder_phospho, 
