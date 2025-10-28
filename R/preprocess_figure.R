7e5db222ebb2a70ab5619973b743ba05daf0d6de
Gabriele Tomè <tome.gabriele@gmail.com>
2025-10-28 10:00:14 +0100

    v0.2.3.3

--- R/preprocess_figure.R
+++ R/preprocess_figure.R
@@ -102,14 +102,6 @@ generate_abundance_plate_plot <- function(proteome_data) {
     stop("Missing sample annotation!")
   }
   
-  type_plate <- list(6 = c(2,3),
-                     12 = c(3,4),
-                     24 = c(4,6),
-                     48 = c(6,8),
-                     96 = c(8,12),
-                     384 = c(16,24),
-                     1536 = c(32,48))
-  
   c_anno <- copy(proteome_data$c_anno)
   psm_sig_prot_df <- copy(proteome_data$psm_log_prot_df)
   psm_sig_prot_df[, ID_peptide := NULL]
